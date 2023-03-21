1ac953f8556524690724250aa1c7749b46eb12a5
SH <gram753@naver.com>
2023-03-21 14:20:17 +0900

    [이승환] feat(exception) : 커스텀 예외 생성 및 에러코드 정의

---
+++ src/main/java/com/fastcampus/schedule/exception/constant/ErrorCode.java
@@ -0,0 +1,30 @@
+package com.fastcampus.schedule.exception.constant;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+
+	// TODO message
+	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, ""),
+	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
+	DUPLICATED_EMAIL(HttpStatus.CONFLICT, ""),
+	DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ""),
+	;
+
+	private final HttpStatus status;
+	private final String message;
+
+	ErrorCode(HttpStatus status, String message) {
+		this.status = status;
+		this.message = message;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
