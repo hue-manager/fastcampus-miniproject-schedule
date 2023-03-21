57c6b85f208abfee5b530ec8a8fee6a340b8a702
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-03-21 15:41:30 +0900

    [조유성]feat(constant.ErroCode) :에러코드 문구수정
    에러코드 문구 수정

--- src/main/java/com/fastcampus/schedule/exception/constant/ErrorCode.java
+++ src/main/java/com/fastcampus/schedule/exception/constant/ErrorCode.java
@@ -7,9 +7,10 @@ public enum ErrorCode {
 	// TODO message
 	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, ""),
 	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
 	DUPLICATED_EMAIL(HttpStatus.CONFLICT, ""),
 	DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ""),
+
 	;
 
 	private final HttpStatus status;
