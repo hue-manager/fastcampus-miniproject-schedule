54624b318a3816f1e9c422955ce7fedcaea098c7
dont-octopus <jubsuny@gmail.com>
2023-03-29 13:49:07 +0900

    [조윤영] feat(user.test) : userEditInfo 테스트 추가

--- src/test/java/com/fastcampus/schedule/user/controller/SignUpControllerTest.java
+++ src/test/java/com/fastcampus/schedule/user/controller/SignUpControllerTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.fastcampus.schedule.user.controller.requset.UserInfoRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,17 +42,60 @@ class SignUpControllerTest {
 		   .andExpect(status().isOk());
 	}
 
-	// @Test
-	// @DisplayName("회원가입시 이미 가입된 유저니름으로 회원가입 하는경우 에러 반환")
-	// public void existUserName_SignUp_ErrorReturn() throws Exception {
-	// 	String userName = "haribo";
-	// 	String email = "aaa@mail.com";
-	// 	String password = "password";
-	//
-	// 	mvc.perform(post("/user/signUp")
-	// 					.contentType(MediaType.APPLICATION_JSON)
-	// 					.content(objectMapper.writeValueAsBytes(new SignUpUserRequest(userName, email, password)))
-	// 	   ).andDo(print())
-	// 	   .andExpect(status().isConflict());
-	// }
+	@Test
+	public void userEditInfoTest() throws Exception{
+
+		String userName = "haribo";
+		String email = "aaa@mail.com";
+		String password = "password01";
+
+
+		mvc.perform(post("/signup")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsBytes(new SignUpRequest(email,
+								userName,
+								password,
+								"010-1111-2222"))))
+				.andDo(print())
+				.andExpect(status().isOk());
+
+		String email2 = "bbb@gmail.com";
+		String userName2 = "test";
+
+		mvc.perform(post("/users/1/editinfo")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content(objectMapper.writeValueAsBytes(new UserInfoRequest(email2, userName2)))
+						// email과 username 둘 다 수정할경우는 status 200 뜸
+				).andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void userEditInfoTest2() throws Exception{
+
+		String userName = "haribo";
+		String email = "aaa@mail.com";
+		String password = "password01";
+
+
+		mvc.perform(post("/signup")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsBytes(new SignUpRequest(email,
+								userName,
+								password,
+								"010-1111-2222"))))
+				.andDo(print())
+				.andExpect(status().isOk());
+
+		String email2 = "bbb@gmail.com";
+		String userName2 = "test";
+
+		mvc.perform(post("/users/1/editinfo")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content(objectMapper.writeValueAsBytes(new UserInfoRequest(email2)))
+						// email과 username중 하나만 수정할경우는 status 400 뜸
+						// -> 하나만 받아도 되도록 UserInfoRequest 에서 생성자 추가했지만 왜 실패인지 모르겠음...
+				).andDo(print())
+				.andExpect(status().isOk());
+	}
 }
