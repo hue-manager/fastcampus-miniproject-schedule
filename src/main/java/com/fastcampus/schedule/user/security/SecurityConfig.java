d86c6c618dc99b8f4af60d8d51701ff21b60d67d
SH <gram753@naver.com>
2023-03-29 18:19:28 +0900

    [이승환] feat : update security config

--- src/main/java/com/fastcampus/schedule/user/security/SecurityConfig.java
+++ src/main/java/com/fastcampus/schedule/user/security/SecurityConfig.java
@@ -1,34 +1,44 @@
 package com.fastcampus.schedule.user.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fastcampus.schedule.user.jwt.JwtFilter;
+import com.fastcampus.schedule.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.csrf().disable()
-				   .authorizeRequests()
-				   .antMatchers("/login", "/signup").permitAll()
-				   .antMatchers("/admins/**").hasRole("ROLE_ADMIN")
-				   .anyRequest().authenticated()
-				   .and().build();
-	}
+	private final UserService userService;
+	@Value("${jwt.secret-key}")
+	private String secretKey;
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws
-		Exception {
-		return authenticationConfiguration.getAuthenticationManager();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/login", "/signup").permitAll()
+			.antMatchers("/admins/**").hasRole("ADMIN")
+			.antMatchers("/**").authenticated()
+			.anyRequest().permitAll()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class);
 	}
 }
