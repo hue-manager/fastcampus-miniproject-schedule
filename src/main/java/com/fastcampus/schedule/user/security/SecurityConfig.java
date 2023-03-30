ee61ca661ff8408094be72828304d4ac1991456d
SH <gram753@naver.com>
2023-03-30 13:40:30 +0900

    [이승환] chore : 포트 번호 수정 및 필터 업데이트

--- src/main/java/com/fastcampus/schedule/user/security/SecurityConfig.java
+++ src/main/java/com/fastcampus/schedule/user/security/SecurityConfig.java
@@ -1,19 +1,13 @@
 package com.fastcampus.schedule.user.security;
 
-import com.fastcampus.schedule.user.domain.constant.Role;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fastcampus.schedule.user.jwt.JwtFilter;
@@ -26,55 +20,58 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    private static final String[] PERMIT_URL_ARRAY = {
-            /* swagger v2 */
-            "/v2/api-docs",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
-            /* swagger v3 */
-            "/v3/api-docs/**",
-            "/swagger-ui/**"
-    };
+	private static final String[] PERMIT_URL_ARRAY = {
+		/* swagger v2 */
+		"/v2/api-docs",
+		"/swagger-resources",
+		"/swagger-resources/**",
+		"/configuration/ui",
+		"/configuration/security",
+		"/swagger-ui.html",
+		"/webjars/**",
+		/* swagger v3 */
+		"/v3/api-docs/**",
+		"/swagger-ui/**"
+	};
 
-    @Value("${jwt.secret-key}")
-    private String secretKey;
+	@Value("${jwt.secret-key}")
+	private String secretKey;
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs/**");
-        web.ignoring().antMatchers("/swagger.json");
-        web.ignoring().antMatchers("/swagger-ui.html");
-        web.ignoring().antMatchers("/swagger-resources/**");
-        web.ignoring().antMatchers("/webjars/**");
-    }
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs/**");
+		web.ignoring().antMatchers("/swagger.json");
+		web.ignoring().antMatchers("/swagger-ui.html");
+		web.ignoring().antMatchers("/swagger-resources/**");
+		web.ignoring().antMatchers("/webjars/**");
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().disable()
-                .csrf().disable()
-                .cors().disable()
-                .authorizeRequests()
-                .antMatchers("/login", "/signup", "/").permitAll()
-                .antMatchers(PERMIT_URL_ARRAY).permitAll()
-                .antMatchers("/users/**", "schedules/**").permitAll()
-                .antMatchers("/admins/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class);
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.httpBasic().disable()
+			.csrf().disable()
+			.cors().disable()
+			.authorizeRequests()
+			.antMatchers("/login", "/signup", "/").permitAll()
+			.antMatchers(PERMIT_URL_ARRAY).permitAll()
+			.antMatchers("/users/**", "schedules/**").permitAll()
+			.antMatchers("/admins/**").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+            .exceptionHandling()
+			.authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+			.and()
+			.addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class);
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
-    }
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService);
+	}
 }
