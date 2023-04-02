68efdd0e36980727d0f31cf314693c1ca1d641dc
haribo033 <ehfaksdl033@gmail.com>
2023-04-02 17:56:05 +0900

    [이진혁] SignUpRequest - 비밀번호확인을 위한 password2 필드 추가 SignUpController - 회원가입 exception 추가 테스트 x

--- src/main/java/com/fastcampus/schedule/user/controller/SignUpController.java
+++ src/main/java/com/fastcampus/schedule/user/controller/SignUpController.java
@@ -2,8 +2,10 @@ package com.fastcampus.schedule.user.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +24,27 @@ public class SignUpController {
 	public static final String SIGN_UP_SUCCESS = "회원가입 성공";
 	private final UserService userService;
 
-	@PostMapping("/signup")
+	/*@PostMapping("/signup")
 	public HttpEntity<String> SignUp(@RequestBody @Valid SignUpRequest request) {
 		userService.signUp(request);
 		return ResponseEntity.ok(SIGN_UP_SUCCESS);
-	}
+	}*/
+    @PostMapping("/signup")
+    public HttpEntity<Void> SignUp(@RequestBody @Valid SignUpRequest request, BindingResult result) {
+        if (!request.getPassword().equals(request.getPassword2())) {
+            result.rejectValue("memberPassword2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+        }
 
+        try {
+            userService.signUp(request);
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            result.reject("signupFailed", "이미 등록된 사용자 ID 입니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.reject("signupFailed", e.getMessage());
+        }
+        return ResponseEntity.ok(null);
+    }
 }
--- src/main/java/com/fastcampus/schedule/user/controller/request/SignUpRequest.java
+++ src/main/java/com/fastcampus/schedule/user/controller/request/SignUpRequest.java
@@ -21,36 +21,40 @@ import lombok.Setter;
 @NoArgsConstructor
 public class SignUpRequest {
 
-	@NotNull
-	@Email
-	private String email;
+    @NotNull
+    @Email
+    private String email;
 
-	@NotNull
-	@Length(min = 2, max = 6)
-	@Pattern(regexp = "^[가-힣]{2,6}$")
-	private String userName;
+    @NotNull
+    @Length(min = 2, max = 6)
+    @Pattern(regexp = "^[가-힣]{2,6}$")
+    private String userName;
 
-	@NotNull
-	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,15}$")
-	private String password;
+    @NotNull
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,15}$"
+            , message = "비밀번호는 필수항목입니다, 비밀번호는 영어와 숫자로 포함해서 8~15자리 이내로 입력해주세요.")
+    private String password;
 
-	@NotNull
-	@Pattern(regexp = "^01(?:0|1|6)-(?:\\d{3}|\\d{4})-\\d{4}$")
-	private String phoneNumber;
+    @NotNull
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,15}$")
+    private String password2;
+    @NotNull
+    @Pattern(regexp = "^01(?:0|1|6)-(?:\\d{3}|\\d{4})-\\d{4}$", message = "입력 예 : 010-0000-0000")
+    private String phoneNumber;
 
-	@NotNull
-	private String position;
+    @NotNull
+    private String position;
 
-	@NotNull
-	private String department;
+    @NotNull
+    private String department;
 
-	public static User toEntity(SignUpRequest request, PasswordEncoder encoder) {
-		return User.of(request.email,
-					   request.userName,
-					   encoder.encode(request.password),
-					   request.phoneNumber,
-					   Role.DEFAULT,
-					   request.position,
-					   request.department);
-	}
+    public static User toEntity(SignUpRequest request, PasswordEncoder encoder) {
+        return User.of(request.email,
+                request.userName,
+                encoder.encode(request.password),
+                request.phoneNumber,
+                Role.DEFAULT,
+                request.position,
+                request.department);
+    }
 }
