449abe4dc94c0e8a5cf1c76c28c1292e3b63fccc
SH <gram753@naver.com>
2023-04-03 07:01:17 +0900

    cors 수정

--- src/main/java/com/fastcampus/schedule/config/WebConfig.java
+++ src/main/java/com/fastcampus/schedule/config/WebConfig.java
@@ -8,9 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("*")
-				.allowedMethods("GET", "POST")
-				.maxAge(3000);
+		registry.addMapping("/**");
 	}
 }
